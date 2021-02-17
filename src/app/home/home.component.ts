5bb45c046140611f446927944724eab749a2c1b8
Brent Acs <brent.mann@gmail.com>
2021-02-17 14:36:08 -0500

    EoL 13.173 added custom observable to replace simple first example.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +14,23 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.obserableSubscription = interval(1000).subscribe((count) => {
-      console.log("count: " + count);
-    });
+    // this.obserableSubscription = interval(1000).subscribe((count) => {
+    //   console.log("count: " + count);
+    // });
+
+    const customIntervalObservable = Observable
+      .create( observer => {
+        let count = 0;
+        setInterval( () => {
+          observer.next(count);
+          count++;
+        }, 1000);
+      });
+
+    this.obserableSubscription = customIntervalObservable.subscribe( data => {
+       console.log("data: " + data);
+     })
+
   }
 
   ngOnDestroy(){
